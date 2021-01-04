dcf85bb8f119c98b12000cd240463abf57805eba
Shatab Anwar <shatabanwar11@gmail.com>
2021-01-04 10:31:24 +0530

    Added sounds and images

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-alert("Welcome");
+var length = document.querySelectorAll('.drum').length;
+for(var i = 0; i<length;i++){
+  document.querySelectorAll('.drum')[i].addEventListener("click",
+  function (){
+  var audio = new Audio('sounds/tom-1.mp3');
+  audio.play();
+});
+}
